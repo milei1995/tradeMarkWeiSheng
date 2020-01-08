cd5141061ff22ce5542a9adacee3db51f5761959
milei1995 <381889351@qq.com>
2020-01-08 21:57:19 +0800

    1/8/21:57

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,15 +1,137 @@
 <template>
-  <div>
-  登录页
+  <div class="login">
+    <div class="login-logo">
+      <img class="login-logo-img" src="../../bannerAndIcon/login-logo2.png" />
+    </div>
+    <div class="login-content">
+      <div class="login-content-title">账号登录</div>
+      <a-input class="login-content-user" placeholder="请输入用户名或手机号"></a-input>
+      <a-icon type="user" class="login-content-user-icon" />
+      <a-input class="login-content-possword" placeholder="请输入密码"></a-input>
+      <a-icon type="lock" class="login-content-possword-icon" />
+      <div class="login-content-tip">忘记密码？</div>
+      <a-button class='login-button'>登录</a-button>
+      <div class='toregister'>立即免费注册>>></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "login"
+};
 </script>
 
-<style>
-
+<style scoped>
+.login {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: url("../../bannerAndIcon/login-bg.png") no-repeat center;
+  background-size: cover;
+}
+.login-logo {
+  width: 440px;
+  height: 87px;
+  margin: 120px auto 0px;
+}
+.login-logo-img {
+  width: 100%;
+  height: 100%;
+}
+.login-content {
+  padding-top: 1px;
+  width: 44%;
+  min-width: 650px;
+  height: 510px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  border-radius: 10px;
+  margin: 30px auto 0px;
+  position: relative;
+}
+.login-content-title {
+  margin-top: 45px;
+  margin-left: 18%;
+  width: 15%;
+  height: 5%;
+  font-size: 20px;
+  font-family: Source Han Sans CN;
+  font-weight: bold;
+  line-height: 22px;
+  color: rgba(51, 51, 51, 1);
+  opacity: 1;
+}
+.login-content-user {
+  width: 64%;
+  height: 65px;
+  line-height: 65px;
+  margin-top: 30px;
+  margin-left: 18%;
+  background: rgba(255, 255, 255, 1);
+  border: 1px solid rgba(153, 153, 153, 1);
+  opacity: 1;
+  text-align: center;
+  font-size: 30px;
+}
+.login-content-user-icon {
+  font-size: 30px;
+  position: absolute;
+  left: 20%;
+  top: 115px;
+}
+.login-content-possword {
+  width: 64%;
+  height: 65px;
+  line-height: 65px;
+  margin-top: 30px;
+  margin-left: 18%;
+  background: rgba(255, 255, 255, 1);
+  border: 1px solid rgba(153, 153, 153, 1);
+  opacity: 1;
+  text-align: center;
+  font-size: 30px;
+}
+.login-content-possword-icon {
+  font-size: 30px;
+  position: absolute;
+  left: 20%;
+  top: 211px;
+}
+.login-content-tip {
+  margin-left:18%;
+  margin-top:20px;
+  width: 13%;
+  height: 14px;
+  font-size: 14px;
+  font-family: Source Han Sans CN;
+  font-weight: 400;
+  line-height: 22px;
+  color: rgba(153, 153, 153, 1);
+  opacity: 1;
+}
+.login-button{
+width:64%;
+height:45px;
+line-height:45px;
+background:rgba(255,90,96,1);
+opacity:1;
+margin-left: 18%;
+margin-top:20px;
+color:#ffffff;
+font-size:22px;
+}
+.toregister{
+width:20%;
+height:14px;
+font-size:14px;
+font-family:Source Han Sans CN;
+font-weight:400;
+line-height:22px;
+color:rgba(255,145,72,1);
+opacity:1;
+margin:20px auto 0px;
+}
 </style>
--- src/views/TrademarkRegister/TrademarkRegister.vue
+++ src/views/TrademarkRegister/TrademarkRegister.vue
@@ -1,15 +1,137 @@
 <template>
-  <div>
-    商标注册页
+  <div class="register">
+    <div class="register-logo">
+      <img class="register-logo-img" src="../../bannerAndIcon/login-logo2.png" />
+    </div>
+    <div class="register-content">
+      <div class="register-content-title">手机号注册</div>
+      <div class="register-content-tip1">
+        <span>已有账号？</span>&nbsp;&nbsp;
+        <span style="color:rgba(255,106,7,1);cursor:pointer;">登录</span>
+      </div>
+      <div class="register-content-phone">
+        <a-select defaultValue="lucy" class='register-content-phone-part1'>
+          <a-select-option value="jack">+86</a-select-option>
+          <a-select-option value="lucy">+99</a-select-option>
+        </a-select>
+        <a-input placeholder="请输入机号" class='register-content-phone-part2'></a-input>
+      </div>
+      <div class='register-content-vcode'>
+        <a-input placeholder="请输入验证码" style='width:40%;' class='register-content-vcode-part1'></a-input>
+        <a-button  style='width:55%;' class='register-content-vcode-part2'>发送验证码</a-button>
+      </div>
+      <div class='register-content-password'>
+        <a-input style='width：100%;' placeholder='设置密码'></a-input>
+        <a-icon type="eye" class="register-content-password-icon" />
+      </div>
+      <div class='register-content-to'>
+        <a-button class='register-content-to-button'>立即注册</a-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "TrademarkRegister"
+};
 </script>
 
-<style>
-
+<style scoped>
+.register {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: url("../../bannerAndIcon/register-bg.png") no-repeat center;
+  background-size: cover;
+}
+.register-logo {
+  width: 440px;
+  height: 87px;
+  margin: 120px auto 0px;
+}
+.register-logo-img {
+  width: 100%;
+  height: 100%;
+}
+.register-content {
+  padding-top: 1px;
+  width: 44%;
+  border-radius: 10px;
+  /* margin:30px,7.5%,0px,48.5%; */
+  margin-top: 30px;
+  margin-left: 48.5%;
+  min-width: 650px;
+  height: 510px;
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
+  opacity: 1;
+}
+.register-content-title {
+  margin-top: 45px;
+  margin-left: 7%;
+  height: 5%;
+  font-size: 20px;
+  font-family: Source Han Sans CN;
+  font-weight: bold;
+  line-height: 22px;
+  color: rgba(51, 51, 51, 1);
+  opacity: 1;
+}
+.register-content-tip1 {
+  margin-top: 15px;
+  margin-left: 7%;
+  font-size: 14px;
+  font-family: Source Han Sans CN;
+  font-weight: 500;
+  line-height: 22px;
+  color: rgba(51, 51, 51, 1);
+  opacity: 1;
+}
+.register-content-phone,.register-content-vcode{
+  margin-top: 15px;
+  margin-left: 7%;
+  margin-right: 8%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+}
+.register-content-phone-part1{
+  width:35%;
+}
+.register-content-phone-part2{
+ width:60%;
+}
+.register-content-vcode-part2{
+  border:2px solid rgba(51,51,51,1);
+}
+.register-content-password{
+   margin-top: 15px;
+  margin-left: 7%;
+  margin-right: 8%;
+  height: 50px;
+  line-height:50px;
+  position:relative;
+}
+.register-content-password-icon{
+  position: absolute;
+  right:20px;
+  font-size:20px;
+  top:16px;
+}
+.register-content-to{
+  margin-top: 15px;
+  margin-left: 7%;
+  margin-right: 8%;
+  height: 50px;
+  line-height:50px;
+}
+.register-content-to-button{
+  width:100%;
+ background:rgba(255,90,96,1);
+ opacity:1;
+ color:#ffffff;
+}
 </style>
