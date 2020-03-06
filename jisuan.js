67103377f7770661eebcee3de6de98762b6e1742
milei1995 <381889351@qq.com>
2020-03-06 17:27:43 +0800

    3.6

---
+++ jisuan.js
@@ -0,0 +1,28 @@
+const selected = [{
+        className: '办公用品',
+        classNum: '16',
+        chooseGroup: [{
+                groupNum: '1607',
+                groupArray: ['商品1', '商品2', '商品3']
+            },
+            {
+                groupNum: '1603',
+                groupArray: ['商品4', '商品5']
+            }
+        ],
+        totalArray: []
+    }, {
+        className: '服装鞋帽',
+        classNum: '17',
+        chooseGroup: [{
+                groupNum: '1502',
+                groupArray: ['商品6', '商品7', '商品8']
+            },
+            {
+                groupNum: '1603',
+                groupArray: ['商品9', '商品10']
+            }
+        ],
+        totalArray: []
+    }
+]
--- src/components/tradeMarkCategory/index.vue
+++ src/components/tradeMarkCategory/index.vue
@@ -7,7 +7,7 @@
             class="category1-item"
             v-for="(item,index) in category1"
             :key="index"
-            @click="chooseTrade(item.classifyNum)"
+            @click="chooseTrade(item.classifyNum,item.classifyName)"
           >第{{item.classifyNum}}类&nbsp;{{item.classifyName}}</div>
         </div>
       </a-tab-pane>
@@ -24,9 +24,17 @@
       <a-tab-pane class="tab-pane" tab="情选择具体商品/服务" key="3">
         <div class="category3">
           <div class="category3-title">
-            <span></span>
+            <span>{{category3.groupsNum}}</span>--
+            <span>{{category3.groupName}}</span>
           </div>
-          <div class="category3-item" v-for="(item,index) in category3" :key="index">{{item}}</div>
+          <div class="category3-content">
+            <div
+              class="category3-item"
+              v-for="(item,index) in category3.goodsList"
+              :key="index"
+              @click="selectGoods(item,category3.groupsNum)"
+            >{{item}}</div>
+          </div>
         </div>
       </a-tab-pane>
     </a-tabs>
@@ -34,31 +42,104 @@
 </template>
 
 <script>
+var groupArray = []; //当前挑选类的小类
+var chooseGroup = []; //当前挑选类的小类的商品
+var currentSelectedClass = {}; //当前挑选的类
 export default {
   data() {
     return {
       category1: [], //存放国际分类
       category2: [], //存放类似群组
       category3: [], //存放具体商品
-      currentActiveKey: "1"
+      currentActiveKey: "1",
+      selected: [] //挑选的整体数组
     };
+  },
+  computed: {
+    
   },
   mounted() {
     this.getCategory(1, "");
   },
   methods: {
     callback(e) {
-      this.currentActiveKey = e;
+      //   this.currentActiveKey = e;
+      if (e === "1") {
+        this.currentActiveKey = "1";
+        currentSelectedClass = {};
+        chooseGroup = [];
+        groupArray = [];
+      }
+      if (e == "2") {
+        console.log(this.selected);
+        groupArray = [];
+        if (this.selected.length === 0) {
+          this.$message.warning("请先选择国际分类");
+        } else {
+          this.currentActiveKey = "2";
+        }
+      }
+      if (e == "3") {
+        if (this.selected.length === 0) {
+          this.$message.warning("请先选择国际分类");
+        } else {
+          if (this.selected[0].chooseGroup) {
+            this.currentActiveKey = "3";
+          } else {
+            this.$message.warning("请选择类似群组");
+          }
+        }
+      }
     },
-    chooseTrade(code) {
+
+    chooseTrade(code, name) {
       this.currentActiveKey = "2";
       this.getCategory(2, code);
+      const isExist = this.selected.find(z => z.classNum == code);
+      console.log(isExist);
+      if (!isExist) {
+        const obj1 = {
+          className: name,
+          classNum: code
+        };
+        currentSelectedClass = { ...obj1 };
+         console.log(currentSelectedClass);
+        this.selected.push(currentSelectedClass);
+      }else{
+        var i;
+        for( i=0;i<isExist.chooseGroup.length;i++){
+           if(isExist.chooseGroup[i].groupsNum==this.category3.groupsNum)
+           return i
+        }
+        console.log(i)
+        groupArray=isExist.chooseGroup[i].groupArray
+      } 
     },
     chooseTrade2(groupsNum) {
       let choosed = this.category2.find(n => n.groupsNum == groupsNum);
       console.log(choosed);
       this.currentActiveKey = "3";
-      this.category3 = choosed.goodsList;
+      this.category3 = choosed;
+      const obj2 = {
+        groupsNum: choosed.groupsNum
+      };
+      const isExist = chooseGroup.find(x => x.groupsNum == groupsNum);
+      if (!isExist) {
+        chooseGroup.push(obj2);
+      } else {
+        groupArray = isExist.groupArray;
+      }
+      currentSelectedClass.chooseGroup = chooseGroup;
+      console.log(currentSelectedClass);
+    },
+    selectGoods(goods, groupsNum) {
+      groupArray.push(goods);
+      const target = currentSelectedClass.chooseGroup.find(
+        n => n.groupsNum == groupsNum
+      );
+      target.groupArray = groupArray;
+      console.log(currentSelectedClass);
+      console.log(this.selected);
     },
     getCategory(queryType, code) {
       const url =
@@ -96,13 +177,20 @@ export default {
       width: 100%;
       .category1 {
         width: 100%;
+        height:300px;
+        overflow-y:scroll ;
         display: flex;
+        align-content:flex-start;
         flex-wrap: wrap;
         .category1-item {
           width: 14%;
           height: 30px;
           cursor: pointer;
           line-height: 30px;
+        }
+        .active1 {
+          color: #ffffff;
+          background-color: #faa80a;
         }
         .category1-item:hover {
           background-color: #faa80a;
@@ -111,12 +199,15 @@ export default {
       }
       .category2 {
         width: 100%;
+        height:300px;
+        overflow-y:scroll ;
         display: flex;
+        align-content:flex-start;
         flex-wrap: wrap;
         .category2-item {
           width: 50%;
-          padding-left:20px;
-          overflow:hidden;
+          padding-left: 20px;
+          overflow: hidden;
           height: 30px;
           cursor: pointer;
           line-height: 30px;
@@ -126,20 +217,33 @@ export default {
           color: #ffffff;
         }
       }
-      .category3{
+      .category3 {
         width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        .category3-item {
-          background-color:#ECECEC;
-          margin-left:20px;
-          margin-top:10px;
-          cursor: pointer;
-          line-height: 30px;
+        .category3-title {
+          width: 100%;
+          // height:50px;
+          text-align: center;
+          font-size: 28px;
         }
-        .category3-item:hover {
-          background-color: #faa80a;
-          color: #ffffff;
+        .category3-content {
+          height:300px;
+          overflow-y:scroll ;
+          width: 100%;
+          display: flex;
+          align-content:flex-start;
+          flex-wrap: wrap;
+          .category3-item {
+            background-color: #ececec;
+            margin-left: 20px;
+            margin-top: 10px;
+            height:30px;
+            cursor: pointer;
+            line-height: 30px;
+          }
+          .category3-item:hover {
+            background-color: #faa80a;
+            color: #ffffff;
+          }
         }
       }
     }
